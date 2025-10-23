91e410aba37c4ef11296f2edac0492a9bc7c5892
Nihal Afsal <nihalafs11@users.noreply.github.com>
2025-10-22 20:32:26 -0400

    Update NumberTrianglePattern.java

--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class NumberTrianglePattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a positive integer:");
+        System.out.println("Enter a positive integer:");
         int length = sc.nextInt();
         int number = length;
 
